65c09759ac665b331f0695be230d75c09f1f3317
americanchanel02-oss <americanchanel02@gmail.com>
2026-02-21 10:49:47 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,13 @@
 TOKEN = "SENING_BOT_TOKEN"   #8599100876:AAGhk-U0gLCKNUAEf5Q1qThzsaAH-WHYmmA
 ADMIN_ID = 123456789         #7257755738
 
-# Bu yerga Telegram bot kodi yoziladi
+# import telebot
+
+bot = telebot.TeleBot(TOKEN)
+
+# Foydalanuvchi /start yuborsa xabar
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Salom! Kino botga xush kelibsiz 🎬")
+
+bot.polling()Bu yerga Telegram bot kodi yoziladi
